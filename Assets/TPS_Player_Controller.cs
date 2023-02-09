6365d1b3d72aba9708ca7010780d97d47433249c
Oscaro55 <oscaroduponchel@gmail.com>
2023-02-09 22:26:47 +0100

    oscar test

---
+++ Assets/TPS_Player_Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPS_Player_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ApplyMovement(float x, float y)
+    {
+
+    }
+}
