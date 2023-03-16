0657746c6614e58c21b376992aa018c03b9a81cf
Oscaro55 <oscaroduponchel@gmail.com>
2023-03-16 13:03:23 +0100

    spoinw

--- Assets/Controller_Rumble.cs
+++ Assets/Controller_Rumble.cs
@@ -8,21 +8,22 @@ public class Controller_Rumble : MonoBehaviour
     PlayerIndex playerIndex;
     GamePadState state;
     GamePadState prevState;
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("joystick button 0"))
+        if (Vector3.Distance(transform.position, player.position) < 30)
         {
-            GamePad.SetVibration(playerIndex, 0.4f, 0.4f);
+            GamePad.SetVibration(playerIndex, 0.8f - Vector3.Distance(transform.position, player.position) / 30, 0.8f - Vector3.Distance(transform.position, player.position) / 30);
         }
 
-        if (Input.GetKeyDown("joystick button 1"))
+        if (Vector3.Distance(transform.position, player.position) > 30)
         {
             GamePad.SetVibration(playerIndex, 0, 0);
         }
